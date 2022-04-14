1e307c467d1993806c692ff2d395d5ac8e68a6ac
Chad Cunningham <chadcunningham7676@gmail.com>
2022-04-14 16:04:25 -0400

    Fixed Syntax

--- src/asteroid.js
+++ src/asteroid.js
@@ -3,14 +3,18 @@ const MovingObject = require("./moving_object");
 
 const DEFAULTS = {
   COLOR: "#505050",
-  RADIUS: 25
+  RADIUS: 25,
+  SPEED: 4
 };
 
-function Asteroid(pos){
-  MovingObject.call(this, pos);
-  this.radius = DEFAULTS["RADIUS"];
-  this.color = DEFAULTS["COLOR"];
-  this.vel = Util.randomVec(4);
+function Asteroid(options){
+  this.pos = options.pos;
+  MovingObject.call(this, options);
+  this.radius = DEFAULTS.RADIUS;
+  this.color = DEFAULTS.COLOR;
+  this.vel = Util.randomVec(DEFAULTS.SPEED);
 }
 
 Util.inherits(Asteroid, MovingObject);
+
+module.exports = Asteroid;
