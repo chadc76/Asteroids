08eb8b668563bae51fbbf1757ae5697372a19348
Chad Cunningham <chadcunningham7676@gmail.com>
2022-04-14 15:17:42 -0400

    Added event Listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 const MovingObject = require("./moving_object.js");
 
 window.MovingObject = MovingObject;
+
+document.addEventListener("DOMContentLoaded", function() {
+  let ctx = document.getElementById('game-canvas').getContext('2d');
+  let circle = new MovingObject({
+    pos: [30, 30],
+    vel: [10, 10],
+    radius: 5,
+    color: "#00FF00"
+  });
+
+  circle.draw(ctx);
+});
