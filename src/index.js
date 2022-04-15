1f489e915527a68bd4bee6ff2c68880654ec1203
Chad Cunningham <chadcunningham7676@gmail.com>
2022-04-15 01:39:40 -0400

    Updarted start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 const Game = require("./game.js");
 const GameView = require("./game_view.js");
-const MovingObject = require("./moving_object.js");
-const Ship = require("./ship.js");
 
 document.addEventListener("DOMContentLoaded", function() {
-  let ctx = document.getElementById('game-canvas').getContext('2d');
-  let circle = new MovingObject({
-    pos: [30, 30],
-    vel: [10, 10],
-    radius: 5,
-    color: "#00FF00"
-  });
-  
-  let gameView = new GameView(new Game(), ctx);
-  gameView.start();
+  const canvasEl = document.getElementsByTagName("canvas")[0];
+  canvasEl.width = Game.DIM_X;
+  canvasEl.height = Game.DIM_Y;
+
+  const ctx = canvasEl.getContext("2d");
+  const game = new Game();
+  new GameView(game, ctx).start();
 });
