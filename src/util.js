332909d3d9255a6f1b7bbfc84d555cbe5ec74028
Chad Cunningham <chadcunningham7676@gmail.com>
2022-04-15 17:17:37 -0400

    added helpers for bullet vel adn dir

--- src/util.js
+++ src/util.js
@@ -27,6 +27,15 @@ const Util = {
     return Math.sqrt(
       Math.pow(pos1[0] - pos2[0], 2) + Math.pow(pos1[1] - pos2[1], 2)
     );
+  },
+
+  norm(vec) {
+    return Util.dist([0, 0], vec);
+  },
+
+  dir(vec) {
+    const norm = Util.norm(vec);
+    return Util.scale(vec, 1 / norm);
   }
 }
 
