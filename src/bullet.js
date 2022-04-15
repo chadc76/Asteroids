6b5683fac6aec3594d2d5b640e6cc2805ee76026
Chad Cunningham <chadcunningham7676@gmail.com>
2022-04-15 16:44:46 -0400

    Added Bullet class

---
+++ src/bullet.js
@@ -0,0 +1,15 @@
+const Util = require("./util.js");
+const MovingObject = require("./moving_object.js");
+
+function Bullet(options) {
+  this.pos = options.pos;
+  options.color = options.color;
+  options.radius = 1;
+  options.vel = options.vel;
+
+  MovingObject.call(this, options);
+};
+
+Util.inherits(Bullet, MovingObject);
+
+module.exports = Bullet;
