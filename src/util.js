49bc639c4528929f71c4a72e3f4f146fe1243bb5
Chad Cunningham <chadcunningham7676@gmail.com>
2022-04-14 22:03:36 -0400

    Added dist function

--- src/util.js
+++ src/util.js
@@ -20,6 +20,12 @@ const Util = {
     } else {
       return pos;
     }
+  },
+
+  dist(pos1, pos2) {
+    return Math.sqrt(
+      ((pos1[0] - pos2[0]) ** 2) + ((pos1[1] - pos2[1]) ** 2)
+    );
   }
 }
 
