693ee8840f842470d778d336ed633f4f23b96bf8
Chad Cunningham <chadcunningham7676@gmail.com>
2022-04-14 22:06:31 -0400

    Fixed Dist function

--- src/util.js
+++ src/util.js
@@ -24,7 +24,7 @@ const Util = {
 
   dist(pos1, pos2) {
     return Math.sqrt(
-      ((pos1[0] - pos2[0]) ** 2) + ((pos1[1] - pos2[1]) ** 2)
+      Math.pow(pos1[0] - pos2[0], 2) + Math.pow(pos1[1] - pos2[1], 2)
     );
   }
 }
