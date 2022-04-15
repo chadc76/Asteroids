39675f11ff389c7b3f00b0339dba94249db3696e
Chad Cunningham <chadcunningham7676@gmail.com>
2022-04-15 00:58:12 -0400

    Added bindKey HAndlers

--- dist/main.js
+++ dist/main.js
@@ -243,6 +243,23 @@
 	  this.ship = this.game.addShip();
 	}
 
+	GameView.MOVES = {
+	  w: [0, -1],
+	  a: [-1, 0],
+	  s: [0, 1],
+	  d: [1, 0], 
+	}
+
+	GameView.prototype.bindKeyHandlers = function () {
+	  const ship = this.ship;
+
+	  Object.keys(GameView.MOVES).forEach(function(k) {
+	    const move = GameView.MOVES[k];
+	    key(k, function() { ship.power(move)});
+	  });
+	};
+
+
 	GameView.prototype.start = function() {
 	  let game = this.game;
 	  let ctx = this.ctx;
